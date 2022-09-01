29005773ebe75bdd9f47dba31dce0be3664687c3
jacoobanderson <j00cwa@gmail.com>
2022-09-01 22:26:49 +0200

    Create fetch of advice

--- greeting.js
+++ greeting.js
@@ -1,4 +1,11 @@
 import readline from 'readline'
+import fetch from 'node-fetch'
+
+async function getAdvice () {
+    const response = await fetch('https://api.adviceslip.com/advice')
+    const advice = await response.json()
+    console.log(advice.slip.advice)
+}
 
 const read = readline.createInterface({
     input: process.stdin,
@@ -8,13 +15,16 @@ const read = readline.createInterface({
 read.question('What is your name?', name => {
     let count = 0;
     const loader = setInterval(() => {
-        read.write(` \r${count++}%`)
+        if (count < 101) {
+            process.stdout.write(` \r${count++}%`)
+        }
     }, 25);
 
     setTimeout(() => {
         clearInterval(loader);
         read.write(` \nGreetings ${name} `)
-    }, 2625);
+        getAdvice()
+    }, 3000);
 
     read.close()
 })
